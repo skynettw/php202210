4a6a1593937571a982986dda32e614ae552bd7a0
Richard <skynet.tw@gmail.com>
2022-10-22 17:13:20 +0800

    first commit

---
+++ roadnews.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>馬路消息</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <h2 class="alert alert-primary">馬路消息</h2>
+      <hr>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "12345678";
+            $dbname = "newsdb";
+            
+            // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($conn->connect_error) {
+              die("Connection failed: " . $conn->connect_error);
+            }
+            $sql = "SELECT id, news, pdate FROM roadnews order by pdate desc";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+              while($row = $result->fetch_assoc()) {
+                echo  "id: "   . $row["id"] . 
+                      "News: " . $row["news"] . 
+                      "Pdate:" . $row["pdate"] . "<br>";
+              }
+            } else {
+              echo "0 results";
+            }
+            $conn->close();
+        ?>
+        <hr>
+        <a class="btn btn-warning btn-sm" href="/php202210/">Back</a>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
